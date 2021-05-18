7caecd1d454c7c9077002438bc9aa70ebec408f1
desirel <two_somang@icloud.com>
2021-05-18 09:11:23 +0900

    feat: Memo List 구현

--- SwiftUIMemo/MemoListScene.swift
+++ SwiftUIMemo/MemoListScene.swift
@@ -9,15 +9,23 @@
 import SwiftUI
 
 struct MemoListScene: View {
+    @EnvironmentObject var store: MemoStore
+
+
     var body: some View {
-        Text("Hello, world!")
-                .padding()
+        NavigationView{
+            List(store.list) { memo in
+                Text(memo.content)
+            }
+            .navigationBarTitle("내 메모")
+        }
     }
 }
 
 class MemoListScene_Previews: PreviewProvider {
     static var previews: some View {
         MemoListScene()
+                .environmentObject(MemoStore())
     }
 
     #if DEBUG
