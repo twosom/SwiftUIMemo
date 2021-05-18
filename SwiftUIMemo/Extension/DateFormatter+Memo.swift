a7725594c12f4556f6f40d9dd6db757682349583
desirel <two_somang@icloud.com>
2021-05-18 09:20:57 +0900

    feat: list design

---
+++ SwiftUIMemo/Extension/DateFormatter+Memo.swift
@@ -0,0 +1,19 @@
+//
+// Created by Desire L on 2021/05/18.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let memoDateFormatter: DateFormatter = {
+        let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .none
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
+}
+
+extension DateFormatter: ObservableObject {
+
+}
--- SwiftUIMemo/MemoListScene.swift
+++ SwiftUIMemo/MemoListScene.swift
@@ -10,14 +10,23 @@ import SwiftUI
 
 struct MemoListScene: View {
     @EnvironmentObject var store: MemoStore
+    @EnvironmentObject var formatter: DateFormatter
 
 
     var body: some View {
-        NavigationView{
+        NavigationView {
             List(store.list) { memo in
-                Text(memo.content)
-            }
-            .navigationBarTitle("내 메모")
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                            .font(.body)
+                            .lineLimit(1)
+
+                    Text("\(memo.insertDate, formatter: self.formatter)")
+                    .font(.caption)
+                    .foregroundColor(Color(UIColor.secondaryLabel))
+
+                }
+            }.navigationBarTitle("내 메모")
         }
     }
 }
@@ -26,6 +35,7 @@ class MemoListScene_Previews: PreviewProvider {
     static var previews: some View {
         MemoListScene()
                 .environmentObject(MemoStore())
+                .environmentObject(DateFormatter.memoDateFormatter)
     }
 
     #if DEBUG
